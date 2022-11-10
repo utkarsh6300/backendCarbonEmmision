4061c6be3cb66952b7e8c6dc5c9ed99633047a56
utkarsh yadav <utkarshyadav9876@gmail.com>
2022-11-10 18:22:01 +0530

    Update compare.js

--- routes/api/compare.js
+++ routes/api/compare.js
@@ -25,31 +25,32 @@ var program1 = {
               "clientSecret":process.env.clientSecret
 };
 
+
    
     const config = {
         headers: {
               'Content-Type': 'application/json'
         }
       };
-    if(data[0]===data[1]) res.send({ratio:1});
+    if(data[0]===data[1]) return res.send({energy1:0,
+      energy2:0,ratio:1});
     const res1 = await axios.post('https://api.jdoodle.com/v1/execute',program, config);
     const res2 = await axios.post('https://api.jdoodle.com/v1/execute',program1, config);
 var data1=res1.data;
 var data2=res2.data;
-    const energy1 = (data1.cpuTime * (4 * 12 + 8 * 0.3725) * 1.67 * 708.2); // divide by thousand
-    const energy2 = (data2.cpuTime * (4 * 12 + 8 * 0.3725) * 1.67 * 708.2) ; // divide by thousand
+console.log(data1,data2)
+if(data1.statusCode!=200) return res.status(data1.statusCode).send({message:data1.output,code:"leftside"});
+if(data2.statusCode!=200) return res.status(data2.statusCode).send({message:data2.output,code:"rightside"});
+    const energy1 = (data1.cpuTime * (4 * 12 + 8 * 0.3725) * 1.67 * 708.2)/1000 ; // divide by thousand
+    const energy2 = (data2.cpuTime * (4 * 12 + 8 * 0.3725) * 1.67 * 708.2)/1000 ;// divide by thousand
  var ratio=energy1/energy2;
 
-    //  var ratio=res1.data.cpuTime*res1.data.memory/(res2.data.cpuTime*res2.data.memory);
-    // var ans=475*energy;  //in grams
       res.status(200).send({energy1:energy1,
         energy2:energy2,ratio:ratio});
-    
-    // console.log(res1.data,res2.data);
-    //   res.status(200).send(res1.data);
+   
 } catch (error) {
     console.error(error);
-    res.status(500).send('server error');
+    res.status(500).send({message:"server error",code:"don't know"});
 }
 
 
