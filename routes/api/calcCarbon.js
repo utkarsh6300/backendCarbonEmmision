0df68132e511f185793cca29490344acb58c2bd5
utkarsh yadav <utkarshyadav9876@gmail.com>
2022-11-10 18:20:09 +0530

    Update calcCarbon.js

--- routes/api/calcCarbon.js
+++ routes/api/calcCarbon.js
@@ -6,10 +6,9 @@ router.post('/',[],
 async(req,res)=>{
 
 var data=req.body.data;
-// console.log(req.body);
-// console.log(dataCarbonIntensity);
+
 try {
-    // var PUE=data.PUE;
+    
     var PUE=1.67;
     var runtime=data.runtime;
     var NoOfCores=data.cores;
@@ -18,19 +17,16 @@ try {
     var country1=data.country;
     var countryCarbonIntensity=1;
     dataCarbonIntensity.forEach((country)=>{
-        // console.log(country.carbonIntensity);
+       
 if(country.Region==country1) countryCarbonIntensity=country.carbonIntensity;
     });
-        // console.log(countryCarbonIntensity);
+       
 
     const energy = (runtime * (NoOfCores * tdpc + sizeOfMemory * 0.3725) * 1.67 * countryCarbonIntensity) / 1000;
-    // var coreUsageFactor=data.coreUsageFactor;
-    // var PowerDrawComputingCore=data.PowerDrawComputingCore;
-    // var PowerDrawMemory=data.PowerDrawMemory;
-// change variables to code file size,running time,runtime memory used;
-    // var energy=0.001*PUE*running_time*((NoOfCores*PowerDrawComputingCore*coreUsageFactor)+(sizeOfMemory*PowerDrawMemory));
-    var ans=475*energy;  //in grams
-      res.status(200).send({carbon:ans});
+    const treeMonth=energy/0.9134;
+    const distance=energy/175;
+    var ans=energy;  //in grams
+      res.status(200).send({carbon:ans,distance:distance,treeMonth:treeMonth});
 } catch (error) {
     console.error(error);
     res.status(500).send('server error');
